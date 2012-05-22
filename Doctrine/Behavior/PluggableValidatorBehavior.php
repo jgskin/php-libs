4578455a8a2cad6c0574e262d1629f458d4bf263
Jessé Alves Galdino <galdino.jesse@gmail.com>
2012-05-22 10:00:29 -0300

    * PluggableValidatorBehavior Refactor

--- Doctrine/Behavior/PluggableValidatorBehavior.php
+++ Doctrine/Behavior/PluggableValidatorBehavior.php
@@ -7,40 +7,7 @@ use \Jg\Util\ObjectDataHolder\Manager;
 
 class PluggableValidatorBehavior extends \Doctrine_Template
 { 
-  public function getHolder()
-  {
-    $manager = Manager::getInstance();
-    return $manager->getHolder($this->getInvoker());
-  }
-  
-  public function getValidators()
-  {
-    $invoker_holder = $this->getHolder(); 
-    return isset($invoker_holder->pluggable_validators) ? $invoker_holder->pluggable_validators : array();
-  }
-  
-  public function addValidator(BaseValidator $validator)
-  {
-    $invoker_holder = $this->getHolder(); 
-    
-    if (!isset($invoker_holder->pluggable_validators)) {
-      $invoker_holder->pluggable_validators = array();
-    }
-    
-    $invoker_holder->pluggable_validators[$validator::IDENTIFIER] = $validator;
-  }
-  
-  public function removeValidator($validator_identifier)
-  {
-    $invoker_holder = $this->getHolder();
-    if (isset($invoker_holder->pluggable_validators[$validator_identifier])) {
-      unset($invoker_holder->pluggable_validators[$validator_identifier]);
-    }
-  }
-  
-  public function setTableDefinition()
-  {
+  public function setTableDefinition() {
     $this->addListener(new PluggableValidatorListener());
   }
-  
 }
--- Doctrine/Listener/PluggableValidatorListener.php
+++ Doctrine/Listener/PluggableValidatorListener.php
@@ -6,10 +6,19 @@ class PluggableValidatorListener extends \Doctrine_Record_Listener
   public function preValidate(\Doctrine_Event $event)
   {
     $invoker = $event->getInvoker();
-    
-    if ($validators = $invoker->getValidators()) {
-      foreach ($validators as $validator) {
-        $validator->validate();
+
+    if (count($invoker->_validators)) {
+      foreach ($invoker->_validators as $validator) {
+        //o padrão é que as validações se encontrem na própria classe
+        if (!is_array($validator)) {
+          $validator = array($invoker, $validator);
+        }
+
+        if (!is_callable($validator)) {
+          throw new \RuntimeException('Favor verificar as regras de validação');
+        }
+
+        call_user_func($validator);
       }
     }
   }
