4dce45047fc0536afdc9bd81dc14c52ea2eeb3c3
Jessé Alves Galdino <galdino.jesse@gmail.com>
2012-06-06 16:36:55 -0300

    * Class to convert form errors

---
+++ Symfony/Template/TemplateErrors.php
@@ -0,0 +1,56 @@
+<?php
+namespace Jg\Symfony\Template;
+
+class TemplateErrors
+{
+  static public function getErrors(\sfForm $form, $title = null)
+  {
+    if (!$form->hasErrors()) {
+      return null;
+    }
+
+    return self::buildErrors($form->getErrorSchema(), $form, $title);
+  }
+
+  static protected function buildErrors($errors, $form, $title = null)
+  {
+    $info = array();
+
+    if ($title !== null) {
+      $info['title'] = $title;
+    } elseif ($form instanceof \sfFormObject) {
+      $info['title'] = (string) $form->getObject();
+    }
+
+    foreach ($errors as $key => $error) {
+      if ($error instanceof \sfValidatorErrorSchema) {
+	try {
+	  $info['next'][] = self::buildErrors($error, $form->getEmbeddedForm($key));
+	} catch (\InvalidArgumentException $e) {
+	  throw new RuntimeException("Misconfiguration of error form");
+	}
+      } else {
+	$info['errors'][$key]['message'] = $error->getMessage();
+	if (isset($form[$key])) {
+	  $info['errors'][$key]['label'] = 
+	    $label = $form->getWidget($key)->getLabel() ? $label : $form->getWidgetSchema()->getFormFormatter()->generateLabelName($key);
+	}	
+      }
+    }
+    
+    if (isset($info['next'])) {
+      $new_next = array();
+      foreach ($info['next'] as $next) {
+	if (isset($next['next']) && count($next) == 1) {
+	  $new_next = array_merge($new_next, $next['next']);
+	} else {
+	  $new_next[] = $next;
+	}
+      }
+
+      $info['next'] = $new_next;
+    }
+
+    return $info;
+  }
+} 
