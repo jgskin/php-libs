fbed1ae9f43616d8f59e42f09d4fb5b128baf066
Jessé Alves Galdino <galdino.jesse@gmail.com>
2012-11-13 15:06:39 -0200

    Method to format a formfield value

--- src/Jg/Form/Field/FormField.php
+++ src/Jg/Form/Field/FormField.php
@@ -19,4 +19,25 @@ class FormField extends \sfFormField
   {
     return $this->form->getObject();
   }
+
+  /**
+   * Recupera o valor do campo formatado pelo widget configurado
+   *
+   * @return string
+   */
+  public function getFormatedValue()
+  {
+    // widget configurado
+    $widget = $this->widget;
+
+    if ($widget->hasOption('formatmethod')) {
+      $method = $widget->getOption('formatmethod');
+
+      // utiliza o método configurado
+      return call_user_func($method, $this->value);
+    }
+
+    // nenhum método para formatação configurado, recupera o valor original
+    return $this->value;
+  }
 }
