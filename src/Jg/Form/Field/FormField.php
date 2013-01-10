ee3523f9ecd46261ea5b317965c8466cc7fc2f9d
Jessé Alves Galdino <galdino.jesse@gmail.com>
2013-01-10 16:01:34 -0200

    [feature] Added Jg\Form\Field\FormField::getIterator

--- src/Jg/Form/Field/FormField.php
+++ src/Jg/Form/Field/FormField.php
@@ -3,21 +3,61 @@ namespace Jg\Form\Field;
 
 class FormField extends \sfFormField
 {
+  /**
+   * Formulário o qual configurou o widget
+   */
   protected $form;
   
+  /**
+   * Insere o formulário
+   */
   public function setForm(\sfForm $form)
   {
     $this->form = $form;
   }
   
+  /**
+   * Recupera o formulário
+   */
   public function getForm()
   {
     return $this->form;
   }
   
+  /**
+   * Recupera o objeto atrelado ao formulário
+   */
   public function getObject()
   {
     return $this->form->getObject();
+  }
+
+  /**
+   * Recupera uma versão iteravel da renderização do widget
+   */
+  public function getIterator($attributes = array())
+  {
+    // verifica a configuração do método iterador
+    if (!is_callable($this->widget->getOption('iterator'))) {
+      throw new Exception('Iterator not configured'); // alterar a exception
+    }
+
+    // formatação do nome do campo caso o mesmo pertença a um widget superior
+    $name = 
+      $this->parent 
+      ? 
+      $this->parent->getWidget()->generateName($this->name)
+      : 
+      $this->name;
+    
+    // executa o método iterador
+    return call_user_func(
+      $this->widget->getOption('iterator'),
+      $name,
+      $this->value,
+      $attributes,
+      $this->error
+      );
   }
 
   /**
